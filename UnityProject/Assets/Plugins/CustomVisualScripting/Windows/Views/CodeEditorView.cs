00bb3ba4dbf5d81401185d97d310a566fde4e407
dimonkayaking <dmmurzaev@mail.ru>
2026-03-25 00:30:25 +0500

    Доработка

--- UnityProject/Assets/Plugins/CustomVisualScripting/Windows/Views/CodeEditorView.cs
+++ UnityProject/Assets/Plugins/CustomVisualScripting/Windows/Views/CodeEditorView.cs
@@ -1,59 +1,53 @@
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace CustomVisualScripting.Windows.Views
 {
     public class CodeEditorView : VisualElement
     {
-        private TextField _codeField;
-        private Label _titleLabel;
+        private TextField _textField;
         
         public string Code
         {
-            get => _codeField?.value ?? string.Empty;
-            set { if (_codeField != null) _codeField.value = value; }
+            get => _textField?.value ?? "";
+            set
+            {
+                if (_textField != null)
+                    _textField.value = value;
+            }
         }
         
         public CodeEditorView()
         {
+            var label = new Label("Код для парсинга:");
+            label.style.marginBottom = 5;
+            label.style.fontSize = 12;
+            label.style.unityFontStyleAndWeight = FontStyle.Bold;
+            label.style.color = Color.white;
+            Add(label);
+            
+            _textField = new TextField
+            {
+                multiline = true,
+                value = "// Введите код здесь\n\npublic class MyClass\n{\n    public void MyMethod()\n    {\n        int x = 10;\n        int y = 20;\n        int z = x + y;\n    }\n}"
+            };
+            _textField.style.flexGrow = 1;
+            _textField.style.backgroundColor = new Color(0.15f, 0.15f, 0.15f);
+            _textField.style.unityTextAlign = TextAnchor.UpperLeft;
+            _textField.style.color = new Color(0.9f, 0.9f, 0.9f);
+            _textField.style.fontSize = 13;
+            _textField.style.fontFamily = "Courier New";
+            
+            Add(_textField);
+            
             style.flexGrow = 1;
-            style.backgroundColor = new StyleColor(new UnityEngine.Color(0.18f, 0.18f, 0.18f));
-            style.paddingTop = 5;
-            style.paddingLeft = 5;
-            style.paddingRight = 5;
-            
-            _titleLabel = new Label("C# Code");
-            _titleLabel.style.fontSize = 14;
-            _titleLabel.style.unityFontStyleAndWeight = FontStyle.Bold;
-            _titleLabel.style.color = Color.white;
-            _titleLabel.style.marginBottom = 5;
-            Add(_titleLabel);
-            
-            _codeField = new TextField();
-            _codeField.multiline = true;
-            _codeField.style.flexGrow = 1;
-            _codeField.style.whiteSpace = WhiteSpace.Normal;
-            _codeField.style.backgroundColor = new StyleColor(new UnityEngine.Color(0.22f, 0.22f, 0.22f));
-            _codeField.style.color = Color.white;
-            _codeField.style.fontSize = 12;
-            _codeField.style.fontFamily = "Courier New";
-            _codeField.value = @"float speed = 5;
-float offset = speed * 0.1f;
-if (speed > 3)
-{
-    transform.position = new Vector3(offset, 0, 0);
-    UnityEngine.Debug.Log(speed);
-}
-else
-{
-    UnityEngine.Debug.Log(0);
-}";
-            
-            Add(_codeField);
+            style.marginLeft = 5;
+            style.marginRight = 5;
         }
         
         public void Clear()
         {
-            _codeField.value = string.Empty;
+            _textField.value = "";
         }
     }
 }
