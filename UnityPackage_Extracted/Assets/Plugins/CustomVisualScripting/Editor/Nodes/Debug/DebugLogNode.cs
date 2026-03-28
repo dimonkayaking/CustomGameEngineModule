1b432796a82801e76b2f5082e066a00121a944b0
dimonkayaking <dmmurzaev@mail.ru>
2026-03-28 22:44:56 +0500

    Все ошибки исправлены, плагин работает, прекрасно переводит из кода в ноды и наоборот с сохранением названий переменных

--- UnityPackage_Extracted/Assets/Plugins/CustomVisualScripting/Editor/Nodes/Debug/DebugLogNode.cs
+++ UnityPackage_Extracted/Assets/Plugins/CustomVisualScripting/Editor/Nodes/Debug/DebugLogNode.cs
@@ -7,13 +7,25 @@ using CustomVisualScripting.Editor.Nodes.Base;
 namespace CustomVisualScripting.Editor.Nodes.Debug
 {
     [System.Serializable, NodeMenuItem("Debug/Debug Log")]
-    public class DebugLogNode : BaseExecutionNode
+    public class DebugLogNode : CustomBaseNode
     {
         public override NodeType NodeType => NodeType.DebugLog;
 
         [Input("message")]
         public object message;
 
+        [Output("output")]
+        public object output;
+
         public override string name => "Debug Log";
+
+        protected override void Process()
+        {
+            output = message;
+            if (message != null)
+            {
+                UnityEngine.Debug.Log($"[VS] {message}");
+            }
+        }
     }
 }
